3fec4cf5f573f08a979f42a07c567e72632f17bb
Allie <allie@codefellows.com>
2018-02-25 10:08:08 -0800

    adding todo-client demo

---
+++ scripts/models/task.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var app = {};
+var __API_URL__ = 'http://localhost:3000';
+
+(function (module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Task(taskObject) {
+    Object.keys(taskObject).forEach(key => this[key] = taskObject[key]);
+  }
+
+  Task.prototype.toHtml = function () {
+    let template = Handlebars.compile($('#task-template').text());
+    return template(this);
+  }
+
+  Task.all = [];
+
+  Task.loadAll = rows => {
+    Task.all = rows.sort((a, b) => b.title - a.title).map(task => new Task(task));
+  }
+
+  Task.fetchAll = callback =>
+    $.get(`${__API_URL__}/tasks`)
+      .then(Task.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  module.Task = Task;
+})(app)
