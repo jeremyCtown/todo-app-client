017b2e30097d501a36978439702370d9825829f8
Allie <allie@codefellows.com>
2018-02-26 15:16:09 -0800

    adding deployed heroku url

--- scripts/models/task.js
+++ scripts/models/task.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var app = {};
-var __API_URL__ = 'http://localhost:3000';
+// var __API_URL__ = 'http://localhost:3000';
+var __API_URL__ = 'https://ag-vs-booklist.herokuapp.com';
 
 (function (module) {
   function errorCallback(err) {
